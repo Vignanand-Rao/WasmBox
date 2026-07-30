1e123cf7be12f20b360a2783e548227c4c611e91
Ashwitha Kola <kolaashwitha@gmail.com>
2026-07-30 21:56:23 +0530

    Complete frontend UI for mid project review

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import "./App.css";
+import Editor from "@monaco-editor/react";
+function App() {
+const [code, setCode] = useState(`print("Welcome to WasmBox!")`
+);
+
+const [output, setOutput] = useState("");
+const [error, setError] = useState("");
+const handleRun = () => {
+
+  setError("");
+
+  setOutput(
+`Running Python Code...
+
+${code}
+
+Execution Completed Successfully.`
+  );
+
+};
+  return (
+    <div className="container">
+
+      <header className="header">
+        <h1>⚡ WasmBox</h1>
+<button className="run-btn" onClick={handleRun}>
+  Run ▶
+</button>
+      </header>
+
+      <main className="main-content">
+
+        <div className="editor-section">
+          <h3>🐍 Python Editor</h3>
+
+      <Editor
+        height="600px"
+        language="python"
+        theme="vs-dark"
+        value={code}
+        onChange={(value) => setCode(value || "")}
+        options={{
+          fontSize: 16,
+          minimap: { enabled: false },
+          automaticLayout: true,
+        }}
+      />
+
+        </div>
+
+        <div className="output-section">
+
+          <div className="console">
+            <h3>🖥 Output</h3>
+            <div className="console-box">
+  <pre>{output}</pre>
+</div>
+          </div>
+
+          <div className="console">
+            <h3>⚠ Errors</h3>
+            <div className="console-box">
+  <pre>{error}</pre>
+</div>
+          </div>
+
+        </div>
+
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
