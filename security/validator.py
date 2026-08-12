6c5b1c9b4542cb818d5eb5e6ed3a05b1611d1cc1
Vignanand Rao K <vignanandrao1735@gmail.com>
2026-08-12 16:57:38 +0530

    Improve syntax error validation

--- security/validator.py
+++ security/validator.py
@@ -1,5 +1,7 @@
 import ast
+
 from security.blocked import BLOCKED_MODULES, BLOCKED_FUNCTIONS
+
 
 class SecurityVisitor(ast.NodeVisitor):
     def __init__(self):
@@ -11,22 +13,36 @@ class SecurityVisitor(ast.NodeVisitor):
                 self.errors.append(
                     f"Blocked Module : {module.name}"
                 )
+
         self.generic_visit(node)
+
     def visit_ImportFrom(self, node):
         if node.module in BLOCKED_MODULES:
             self.errors.append(
                 f"Blocked Module : {node.module}"
             )
+
         self.generic_visit(node)
+
     def visit_Call(self, node):
         if isinstance(node.func, ast.Name):
             if node.func.id in BLOCKED_FUNCTIONS:
                 self.errors.append(
                     f"Blocked Function : {node.func.id}"
                 )
+
         self.generic_visit(node)
+
+
 def validate(code):
-    tree = ast.parse(code)
+    try:
+        tree = ast.parse(code)
+    except SyntaxError as exc:
+        return [
+            f"Syntax Error : {exc.msg} at line {exc.lineno}, column {exc.offset}"
+        ]
+
     visitor = SecurityVisitor()
     visitor.visit(tree)
+
     return visitor.errors
