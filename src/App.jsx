625b6c4e679e4fe3db9083a28a9eb47c4db2666f
Vignanand Rao K <vignanandrao1735@gmail.com>
2026-08-13 00:15:50 +0530

    Connect frontend to deployed backend

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:8000/execute",
+        "https://wasmbox.onrender.com/execute",
         {
           method: "POST",
           headers: {
